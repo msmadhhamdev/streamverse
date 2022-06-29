66113df3d325d7212b9a61127e755ef514ca4055
Adhham Safwan <msmadhham@outlook.com>
2022-06-30 00:22:06 +0530

    minor fix

--- src/components/ErrorBox.js
+++ src/components/ErrorBox.js
@@ -4,7 +4,7 @@ const ErrorBox = ({errorMessage}) => {
     return (
         <div className="error-box">
             <FaWifi style={{fontSize: '1.4em', marginRight: '10px'}} />
-            <p>{errorMessage}, try reloading the page!</p>
+            <p>{errorMessage}, try reloading the page.</p>
         </div>
      );
 }
