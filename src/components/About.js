36cd85aa8f0443fd18f0f452e4f9decad02c5b92
Adhham Safwan <msmadhham@outlook.com>
2022-07-01 13:42:52 +0530

    About page update

--- src/components/About.js
+++ src/components/About.js
@@ -9,11 +9,14 @@ const About = () => {
             <h1>Hi, it's Adhham!</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores repellat amet corrupti suscipit libero dolores? Natus commodi deleniti sequi beatae, eveniet alias sint numquam blanditiis dolores dolore? Quia, recusandae illum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iusto quae, consequuntur ducimus ab dignissimos eaque aperiam voluptatibus quam nam optio sint odio minima commodi sapiente porro magni neque reprehenderit.</p>
             <div className="social-links">
-              <a href="/" title='Adhham | GitHub'><FaGithub /></a>
-              <a href="/" title='Adhham | LinkedIn'><FaLinkedin /></a>
-              <a href="/" title='Adhham | Twitter'><FaTwitter /></a>
-              <a href="https://adhhamdev.netlify.app" title='Adhham | Portfolio Website'><FaIdBadge /></a>
+              <a className='github' href="/" title='Adhham | GitHub'><FaGithub /></a>
+              <a className='linkedin' href="/" title='Adhham | LinkedIn'><FaLinkedin /></a>
+              <a className='twitter' href="/" title='Adhham | Twitter'><FaTwitter /></a>
+              <a className='portfolio' href="https://adhhamdev.netlify.app" title='Adhham | Portfolio Website'><FaIdBadge /></a>
             </div>
+            <footer>
+              <small>Copyrigth &copy; Streamverse 2022</small>
+            </footer>
         </div>
     </div>
   )
