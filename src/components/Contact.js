0ab352e3d20658c144ae3dd57f49b7ac934b4a75
Adhham Safwan <msmadhham@outlook.com>
2022-07-05 13:23:21 +0530

    netlify form fix2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,27 +3,27 @@ import {FaPaperPlane} from 'react-icons/fa'
 const Contact = () => {
   return (
     <div className="Contact">
-        <form name="feedback" autoComplete="on">
+        <form name="contact" autoComplete="on">
             <input type="hidden" name="form-name" value="contact" />
             <div className="user-info-field">
                 <h1>Contact Me</h1>
                 <fieldset>
-                    <legend><label htmlFor="username">Name</label></legend>
-                    <input type="text" id="username" name="username" placeholder="Your name..." autoFocus autoComplete="name" required />
+                    <legend><label htmlFor="name">Name</label></legend>
+                    <input type="text" id="name" name="name" placeholder="Your name..." autoFocus autoComplete="name" required />
                 </fieldset>
                 <fieldset>
-                    <legend><label htmlFor="useremail">Email</label></legend>
-                    <input type="email" id="useremail" name="useremail" placeholder="Your email..." autoFocus autoComplete="email" required />
+                    <legend><label htmlFor="email">Email</label></legend>
+                    <input type="email" id="email" name="email" placeholder="Your email..." autoFocus autoComplete="email" required />
                 </fieldset>
                 <fieldset>
-                    <legend><label htmlFor="userattachment">Attachment (optional)</label></legend>
-                    <input type="file" name="userattachment" id="userattachment" autoFocus />
+                    <legend><label htmlFor="file">Attachment (optional)</label></legend>
+                    <input type="file" name="file" id="file" autoFocus />
                 </fieldset>
             </div>
             <div className="user-comment-field">
                 <fieldset>
                     <legend><label htmlFor="usercomment">What do you want to say?</label></legend>
-                    <textarea name="usercomment" id="usercomment" cols="30" rows="10" placeholder="Your opinion..." autoFocus required></textarea>
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Your opinion..." autoFocus required></textarea>
                 </fieldset>
                 <button id="form-clear-btn" type="reset">Clear</button>
                 <button id="form-submit-btn"><FaPaperPlane /> Send to Adhham</button>
